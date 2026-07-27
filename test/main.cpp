b800349e8c341f3ecce89f252f6c297f5d22c92b
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-07-27 20:59:53 +0200

    update: user input as query

--- test/main.cpp
+++ test/main.cpp
@@ -13,8 +13,12 @@ int main() {
     std::shared_ptr<tablog::Tablog> logger = std::make_shared<tablog::Tablog>();
     logger->configure("TQL-test", true);
 
+    std::string query;
+    std::cout << "Please enter a query: ";
+    std::getline(std::cin, query);
+        
     Lexer lexer;
-    std::vector<Token> tokens = lexer.tokenize("SELECT  ColumnName WHERE 1 = 1");
+    std::vector<Token> tokens = lexer.tokenize(query);
     for (int index = 0; index < tokens.size(); index++) {
         logger->log(tablog::DEBUG, "L: " + tokens[index].getLexeme() + " T: " + tokens[index].getTypeAsString());
     }
