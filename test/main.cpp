2d43133bc85653dbe29db32ee0bb4ba9598d6f88
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-09-02 14:36:45 +0200

    update: catch invalid arguments

--- test/main.cpp
+++ test/main.cpp
@@ -17,18 +17,22 @@ int main() {
 
     while (true) {
         query = readline("\033[106;97m TQL \033[0m> ");
-            
-        std::chrono::time_point start = std::chrono::steady_clock::now();
-        std::shared_ptr<arrow::Table> resultTable = queryEngine.execute(query);
-        std::chrono::time_point end = std::chrono::steady_clock::now();
 
-        std::cout << "The query was executed in: "
-                  << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs "
-                  << std::chrono::duration_cast<std::chrono::nanoseconds> (end - start).count() << "ns " << std::endl;
-        std::cout << "Result table: \n" << resultTable->ToString() << std::endl;
+        try {
+            std::chrono::time_point start = std::chrono::steady_clock::now();
+            std::shared_ptr<arrow::Table> resultTable = queryEngine.execute(query);
+            std::chrono::time_point end = std::chrono::steady_clock::now();
 
-        add_history(query);
-        std::free(query);
+            std::cout << "The query was executed in: "
+                      << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs "
+                      << std::chrono::duration_cast<std::chrono::nanoseconds> (end - start).count() << "ns " << std::endl;
+            std::cout << "Result table: \n" << resultTable->ToString() << std::endl;
+
+            add_history(query);
+            std::free(query);
+        } catch (const std::invalid_argument& invalidArgument) {
+            std::cout << "Error: " << invalidArgument.what() << std::endl;
+        }
     }
     
     return 0;
