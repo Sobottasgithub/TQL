48e45849567703b3551c11cb0a429bf5dcab95b7
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-09-03 08:52:49 +0200

    update: version 0.1.0 > 0.1.1

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      version = "0.1.0";
+      version = "0.1.1";
 
       libtablog = tablog.packages.${system}.lib;
 
